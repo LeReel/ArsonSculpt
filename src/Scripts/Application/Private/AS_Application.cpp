d00646ed2b3d1b5fa90fbe52dc3f82fca1e4524c
LeShrexpert <61854276+LeReel@users.noreply.github.com>
2023-05-25 15:21:49 +0200

    Moved Camera functions definitions in cpp

--- src/Scripts/Application/Private/AS_Application.cpp
+++ src/Scripts/Application/Private/AS_Application.cpp
@@ -12,7 +12,7 @@ const unsigned int SCR_HEIGHT = 768;
 float lastFrame = 0.0f;
 float deltaTime = 0.0f;
 
-Camera camera(glm::vec3(0.0f, 0.0f, 25.0f));
+AS_Camera camera(glm::vec3(0.0f, 0.0f, 25.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
