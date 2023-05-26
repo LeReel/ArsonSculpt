f2fdd32d954083d622377b777fc82f826fe573e8
LeShrexpert <61854276+LeReel@users.noreply.github.com>
2023-05-26 11:57:33 +0200

    Created Model class

---
+++ src/Scripts/Application/Private/AS_Model.cpp
@@ -0,0 +1,34 @@
+﻿#include "AS_Model.h"
+
+#include "AS_Mesh.h"
+
+void AS_Model::Draw(AS_Shader& _shader)
+{
+    const unsigned long long _size = meshes.size();
+
+    for (unsigned int i = 0; i < _size; ++i)
+    {
+        meshes[i].Draw(_shader);
+    }
+}
+
+void AS_Model::LoadModel(char* _path)
+{
+}
+
+std::vector<Texture> AS_Model::LoadMaterialTextures(aiMaterial* _material, aiTextureType* _type, std::string _typeName)
+{
+    std::vector<Texture> _v;
+    return _v;
+}
+
+void AS_Model::ProcessNode(aiNode* _node, const aiScene* _scene)
+{
+}
+
+AS_Mesh AS_Model::ProcessMesh(aiMesh* _mesh, const aiScene* _scene)
+{
+    AS_Mesh _m();
+
+    return _m();
+}
---
+++ src/Scripts/Application/Public/AS_Model.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include <vector>
+#include <string>
+
+enum aiTextureType : int;
+struct aiMaterial;
+// Assimp
+struct aiMesh;
+struct aiScene;
+struct aiNode;
+
+struct Texture;
+
+class AS_Mesh;
+class AS_Shader;
+
+class AS_Model
+{
+public:
+
+    AS_Model(char* _path)
+    {
+        LoadModel(_path);
+    }
+    void Draw(AS_Shader& _shader);
+
+private:
+    // Model Data
+    std::vector<AS_Mesh> meshes;
+    std::string directory;
+    
+    void LoadModel(char* _path);
+    std::vector<Texture> LoadMaterialTextures(aiMaterial* _material, aiTextureType* _type, std::string _typeName);
+    
+    void ProcessNode(aiNode* _node, const aiScene* _scene);
+    AS_Mesh ProcessMesh(aiMesh* _mesh, const aiScene* _scene);
+};
