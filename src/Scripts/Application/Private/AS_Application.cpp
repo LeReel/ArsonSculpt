2d39e0b0ede70b92eea96b3a2cbdbf0720b2af0c
LeShrexpert <61854276+LeReel@users.noreply.github.com>
2023-05-26 16:56:34 +0200

    Implemented Model system

--- src/Scripts/Application/Private/AS_Application.cpp
+++ src/Scripts/Application/Private/AS_Application.cpp
@@ -1,13 +1,19 @@
 #include "AS_Application.h"
 
-#include "AS_Shader.h"
 #include "AS_Camera.h"
+
+#include "AS_Model.h"
+
+#include "src/Scripts/filesystem.h"
 
 //Declare window as global otherwise create an unresolved ext. error
 GLFWwindow* window;
 
 const unsigned int SCR_WIDTH = 1024;
 const unsigned int SCR_HEIGHT = 768;
+
+const char* VERTEX_SHADER_PATH = "../src/SimpleVertexShader.glsl";
+const char* FRAGMENT_SHADER_PATH = "../src/SimpleFragmentShader.glsl";
 
 float lastFrame = 0.0f;
 float deltaTime = 0.0f;
@@ -16,13 +22,6 @@ AS_Camera camera(glm::vec3(0.0f, 0.0f, 25.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
-
-// This will identify our vertex array
-GLuint VAOs[2];
-// This will identify our vertex buffer
-GLuint VBOs[2];
-// This will identify our element buffer
-GLuint EBOs[1];
 
 AS_Application::AS_Application()
 {
@@ -84,127 +83,17 @@ void AS_Application::ProcessInput(GLFWwindow* _window)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
+AS_Shader myShader(VERTEX_SHADER_PATH, FRAGMENT_SHADER_PATH);
+//TODO: Add object and(?) configure path
+AS_Model myModel(FileSystem::getPath("resources/objects/backpack/backpack.obj"));
+
 int AS_Application::Run()
 {
     if (Init() == -1)
     {
         return -1;
     }
-
-    GLfloat cubeVertices[] = {
-        -0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, 0.5f, -0.5f,
-        0.5f, 0.5f, -0.5f,
-
-        -0.5f, 0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f, 0.5f,
-        0.5f, -0.5f, 0.5f,
-
-        0.5f, 0.5f, 0.5f,
-        0.5f, 0.5f, 0.5f,
-        -0.5f, 0.5f, 0.5f,
-        -0.5f, -0.5f, 0.5f,
-
-        -0.5f, 0.5f, 0.5f,
-        -0.5f, 0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-
-        -0.5f, -0.5f, 0.5f,
-        -0.5f, 0.5f, 0.5f,
-        0.5f, 0.5f, 0.5f,
-        0.5f, 0.5f, -0.5f,
-
-        0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, 0.5f,
-        0.5f, 0.5f, 0.5f,
-
-        -0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, 0.5f,
-        0.5f, -0.5f, 0.5f,
-
-        -0.5f, -0.5f, 0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, 0.5f, -0.5f,
-        0.5f, 0.5f, -0.5f,
-
-        0.5f, 0.5f, 0.5f,
-        0.5f, 0.5f, 0.5f,
-        -0.5f, 0.5f, 0.5f,
-        -0.5f, 0.5f, -0.5f,
-    };
-
-    //? Create a VertexArrayObject that stores:
-    //? - Calls to glEnable/disableVertexAttribArray
-    //? - Attribute configurations via glVertexAttribPointer
-    //? - VBO associated with vertex attributes by calls to glVertexAttribPointer()
-    glGenVertexArrays(1, VAOs);
-    // Generates buffers, puts the resulting identifier in VBOs
-    glGenBuffers(1, VBOs);
-
-    //1st triangle's configuration
-    glBindVertexArray(VAOs[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-    glBufferData(
-        GL_ARRAY_BUFFER,
-        sizeof(cubeVertices),
-        cubeVertices,
-        GL_STATIC_DRAW
-    );
-    //Position attribute
-    glVertexAttribPointer(
-        0,
-        3,
-        GL_FLOAT,
-        GL_FALSE,
-        3 * sizeof(float),
-        (void*)0);
-    glEnableVertexAttribArray(0);
-    //? Each vertex attribute takes its data from memory managed by a VBO.
-    //? Which VBO it takes its data from is the one bound to GL_ARRAY_BUFFER when calling glVertexAttribPointer()
-    //==============================================================================//
-
-    //==============================================================================//
-    //? Element Buffer Object (EBO) is a buffer that stores indices that GL uses to decide what vertices to draw.
-    //? This is called Indexed Drawing and avoids repeating vertices that overlap (as we draw mainly in triangles)
-    GLuint _indices[] = {
-        0, 1, 3,
-        1, 2, 3
-    };
-
-    // Give our vertices to OpenGL (stores user-defined data into GPU memory; those data are managed by the currently bound buffer)
-    //? GL_STREAM_DRAW:  data is set only once / used at most a few times by the GPU.
-    //? GL_STATIC_DRAW:  data is set only once / used many times by the GPU.
-    //? GL_DYNAMIC_DRAW: data is changed a lot / used many times by the GPU.
-    glGenBuffers(1, &EBOs[0]);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[0]);
-    glBufferData(
-        GL_ELEMENT_ARRAY_BUFFER,
-        sizeof(_indices),
-        _indices,
-        GL_STATIC_DRAW);
-    glVertexAttribPointer(
-        0,
-        3,
-        GL_FLOAT,
-        GL_FALSE,
-        3 * sizeof(float),
-        (void*)0);
-    glEnableVertexAttribArray(0);
-    //==============================================================================//
-
-    //!Do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO;
-    //!Keep the EBO bound.
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    //Modifying other VAOs requires a call to glBindVertexArray
-    //so we generally don't unbind VAOs nor VBOs when it's not directly necessary.
-    glBindVertexArray(0);
-
+    
     // Check if the ESC key was pressed or the window was closed
     while (!glfwWindowShouldClose(window))
     {
@@ -217,6 +106,7 @@ int AS_Application::Run()
         MainLoop();
     }
 
+    //TODO: Implement in objects/meshes ?
     DeallocateAllResources();
 
     glfwTerminate();
@@ -224,62 +114,26 @@ int AS_Application::Run()
     return 0;
 }
 
-glm::vec3 cubePositions[] = {
-    glm::vec3(0.0f, 0.0f, 0.0f),
-    glm::vec3(2.0f, 5.0f, -15.0f),
-    glm::vec3(-1.5f, -2.2f, -2.5f),
-    glm::vec3(-3.8f, -2.0f, -12.3f),
-    glm::vec3(2.4f, -0.4f, -3.5f),
-    glm::vec3(-1.7f, 3.0f, -7.5f),
-    glm::vec3(1.3f, -2.0f, -2.5f),
-    glm::vec3(1.5f, 2.0f, -2.5f),
-    glm::vec3(1.5f, 0.2f, -1.5f),
-    glm::vec3(-1.3f, 1.0f, -1.5f)
-};
-
-void DrawObject(int _index, glm::mat4 _view, glm::vec3 _position, AS_Shader _shader)
-{
-    //! scaling -> rotations -> translations
-
-    //General rendering
-    glm::mat4 _model = glm::mat4(1.0f);
-    const float _angle = 20.0f * _index;
-
-    _model = glm::rotate(_model, glm::radians(_angle), glm::vec3(1.0f, 0.3f, 0.5f));
-    _model = glm::translate(_model, _position);
-
-    //Perspective
-    glm::mat4 _projection = glm::mat4(1.0f);
-    _projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-
-    _shader.Use(); //Every shader and rendering call after this will use given program
-    //? Finding uniform location doesn't require to use the shader program but updating its value does.
-    _shader.SetMat4fv("model", glm::value_ptr(_model));
-    _shader.SetMat4fv("view", glm::value_ptr(_view));
-    _shader.SetMat4fv("projection", glm::value_ptr(_projection));
-
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-}
-
 void AS_Application::MainLoop()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    myShader.Use();
 
-    AS_Shader firstShader("../src/SimpleVertexShader.glsl",
-                          "../src/SimpleFragmentShader.glsl");
-
+    // view/projection transformations
+    glm::mat4 _projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f,
+                                            100.0f);
     glm::mat4 _view = camera.GetViewMatrix();
+    myShader.SetMat4("projection", _projection);
+    myShader.SetMat4("view", _view);
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    // render the loaded model
+    glm::mat4 _model = glm::mat4(1.0f);
+    _model = glm::translate(_model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+    _model = glm::scale(_model, glm::vec3(1.0f, 1.0f, 1.0f)); // it's a bit too big for our scene, so scale it down
+    myShader.SetMat4("model", _model);
 
-    //Any subsequent VBO, EBO, glVertexAttribPtr and glEnableVertexAttribArray calls will be stored inside the currently bound VAO
-    glBindVertexArray(VAOs[0]);
-    for (int i = 0; i < 10; ++i)
-    {
-        DrawObject(i, _view, cubePositions[i], firstShader);
-    }
-
-    glBindVertexArray(NULL);
+    myModel.Draw(myShader);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -387,7 +241,7 @@ int AS_Application::InitGLEW()
 
 void AS_Application::DeallocateAllResources()
 {
-    glDeleteVertexArrays(2, VAOs);
-    glDeleteBuffers(2, VBOs);
+    //glDeleteVertexArrays(2, VAOs);
+    //glDeleteBuffers(2, VBOs);
     //glDeleteProgramsNV(2, shaderPrograms);
 }
